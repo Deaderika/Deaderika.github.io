d1a064b54a80075dc4568f87639eec3847435d83
Deaderika <derikadea87@gmail.com>
2023-12-23 00:18:08 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BeautifulCulture
+Techomfest
---
+++ app.js
@@ -0,0 +1,21 @@
+const inputs = document.querySelectorAll(".contact-input");
+
+inputs.forEach(ipt =>{
+    ipt.addEventListener("focus", () => {
+        ipt.parentNode.classList.add("focus");
+        ipt.parentNode.classList.add("not-empty");
+    });
+    ipt.addEventListener("blur", () => {
+        if(ipt.value ==""){
+            ipt.parentNode.classList.remove("not-empty");
+        }
+        ipt.parentNode.classList.remove("focus");
+    });
+});
+
+let dropdown = document.querySelector('.dropdown');
+let dropdownBtn = document.getElementById('dropdown-btn');
+
+dropdownBtn.addEventListener('click', () => {
+  dropdown.classList.toggle('dropdown-active');
+});
